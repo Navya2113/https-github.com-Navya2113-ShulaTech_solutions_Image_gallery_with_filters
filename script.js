5db1a759cfd742d8b2375d9d07d21833f7b4481e
Navya2113 <navyashreej88@gmail.com>
2025-03-23 22:25:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+//left 
+var swiper=new Swiper('.leftSlider',{
+  slidesPerview:1.3,
+  spaceBetween:10,
+  loop:true,
+  autoplay {
+    delay:1500,
+  },
+
+
+speed:2000,
+direction:'vertical',
+breakpoints:{
+  900:{
+    slidesPerview:1.55,
+  },
+  500:{
+    slidesPerview:2.6,
+  }
+},
+}
+
+);
+
+//center
+
+var swiper=new Swiper('.centerSlider',{
+  slidesPerview:2.1,
+  spaceBetween:10,
+  loop:true,
+  autoplay {
+    delay:2000,
+  },
+
+
+speed:1800,
+direction:'vertical',
+breakpoints:{
+  1200:{
+    slidesPerview:2.2,
+  },
+  900:{
+    slidesPerview:2.25,
+  },
+  500:{
+    slidesPerview:1.9,
+  }
+  
+},
+}
+
+);
+
+//right
+var swiper=new Swiper('.rightSlider',{
+  slidesPerview:1.3,
+  spaceBetween:10,
+  loop:true,
+  autoplay {
+    delay:1500,
+  },
+
+
+speed:2000,
+direction:'vertical',
+breakpoints:{
+  900:{
+    slidesPerview:1.55,
+  },
+  500{
+    slidesPerview:2.6,
+  }
+},
+}
+
+);
+
+
+
+
